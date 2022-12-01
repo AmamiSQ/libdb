f7d7ad16ea27354d076fccd82cafce7edf84d0cb
AmamiSQ <r_zimmerman@outlook.com>
2022-12-01 16:01:51 +0100

    placeholders

--- src/components/bookAdd.js
+++ src/components/bookAdd.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+//import { useState } from "react";
 
 //add books to the database
 export default function BookAdd() {
---
+++ src/components/topButton.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//main attraction, puts owned books on display on a shelf
+export default function TopButton() {
+  return (
+    <div>
+      
+    </div>
+    );
+  }
